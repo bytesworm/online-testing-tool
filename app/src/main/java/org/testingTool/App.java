f434f1c85535d3b08932d1c235b02afb2ba41117
deadshumz <shumzz@bk.ru>
2025-04-19 20:35:05 +0300

    feat: spotlessApply to pass ci

--- app/src/main/java/org/testingTool/App.java
+++ app/src/main/java/org/testingTool/App.java
@@ -3,14 +3,9 @@ package org.testingTool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
-
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    SpringApplication.run(App.class
-
-            , args    );
+    SpringApplication.run(App.class, args);
   }
 }
